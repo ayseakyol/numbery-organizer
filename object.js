e420faaf3492d82d8f55efa14afc172964d91096
ayseakyol <ayseakyol.22309@gmail.com>
2020-03-24 10:44:36 +0100

    finish

--- object.js
+++ object.js
@@ -1,42 +1,65 @@
 const object = {
   numberyStrings: [],
   NaNyStrings: [],
-  isNumberyString: function (param) {
-
+  isNumberyString: function(param) {
+    if (typeof param === "string") {
+      if (isNaN(param)) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
   },
-  addString: function (param) {
-
+  addString: function(param) {
+    if (typeof param === "string") {
+      if (isNaN(param)) {
+        this.NaNyStrings.push(param);
+        return true;
+      } else {
+        this.numberyStrings.push(param);
+        return true;
+      }
+    } else {
+      return false;
+    }
   },
-  allStrings: function () {
-
+  allStrings: function() {
+    return [...this.numberyStrings, ...this.NaNyStrings];
   },
-  evenStrings: function () {
-
+  evenStrings: function() {
+    return this.numberyStrings.filter(number => number % 2 === 0);
   },
-  oddStrings: function () {
-
+  oddStrings: function() {
+    return this.numberyStrings.filter(
+      number => number % 2 === 1 || number % 2 === -1
+    );
   },
-  negativeStrings: function () {
-
+  negativeStrings: function() {
+    return this.numberyStrings.filter(number => number < 0);
   },
-  positiveStrings: function () {
-
+  positiveStrings: function() {
+    return this.numberyStrings.filter(number => number >= 0);
   },
-  zeroStrings: function () {
-
+  zeroStrings: function() {
+    return this.numberyStrings.filter(number => number == 0);
   },
-  numberyAsNumbers: function () {
-
+  numberyAsNumbers: function() {
+    return this.numberyStrings.map(number => Number(number));
   },
-  NaNyAsNumbers: function () {
-
+  NaNyAsNumbers: function() {
+    return this.NaNyStrings.map(number => Number(number));
   },
-  sumOfNumbery: function () {
-
+  sumOfNumbery: function() {
+    return this.numberyStrings.reduce(function(a, b) {
+      return a + Number(b);
+    }, 0);
   },
-  sumOfNaNy: function () {
 
-  },
+  sumOfNaNy: function() {
+    return this.NaNyStrings.reduce(function(a, b) {
+      return a + Number(b);
+    }, 0);
+  }
 };
-
-
